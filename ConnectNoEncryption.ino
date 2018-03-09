b098dbdd0fcd53583823cae1fa121063420127c8
Rachel <32782380+elra2@users.noreply.github.com>
2018-03-09 15:48:44 +0000

    Add files via upload

---
+++ ConnectNoEncryption.ino
@@ -0,0 +1,27 @@
+#include <WiFi.h>
+
+char ssid[] = "ezywifi";     // the name of your network
+int status = WL_IDLE_STATUS;     // the Wifi radio's status
+
+void setup() {
+   // initialize serial:
+   Serial.begin(9600);
+
+   // attempt to connect to an open network:
+   Serial.println("Attempting to connect to open network...");
+   status = WiFi.begin(ssid);
+
+   // if you're not connected, stop here:
+   if ( status != WL_CONNECTED) { 
+     Serial.println("Couldn't get a wifi connection");
+     while(true);
+   } 
+   // if you are connected :
+   else {
+       Serial.print("Connected to the network");
+   }
+}
+
+void loop() {
+   // do nothing
+}
