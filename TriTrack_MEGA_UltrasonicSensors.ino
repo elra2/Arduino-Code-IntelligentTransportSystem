61779b2ac8a5975ccca0178690a849a1a24f9734
Rachel <32782380+elra2@users.noreply.github.com>
2018-05-04 13:28:52 +0100

    Add files via upload

--- TriTrack_MEGA_UltrasonicSensors.ino
+++ TriTrack_MEGA_UltrasonicSensors.ino
@@ -3,10 +3,10 @@
 * and move the tritrack away from it accordingly.
 *  --- 17ELD002, R ASANTE, Group G - Created: 22/01/2018 */
 #include <Servo.h>
-#define L_Trig 4
-#define L_Echo 5
-#define R_Trig 2
-#define R_Echo 3
+#define L_Trig 2
+#define L_Echo 3
+#define R_Trig 4
+#define R_Echo 5
 #define Edge_Trig 6 
 #define Edge_Echo 7
 #define turnleft 8
@@ -15,19 +15,16 @@
 #define forward 11
 #define stoptritrack 12
 
-Servo servo_CH1; //Left leg
-Servo servo_CH2; //right leg
-
 long duration; 
 long distance;
-//reserse is 140
 
 void setup() {
   Serial.begin (9600);
 //Ultrasonic Sensors    
   pinMode(L_Trig, OUTPUT);   
   pinMode(R_Trig, OUTPUT);
-  pinMode(Edge_Trig, OUTPUT);   
+  pinMode(Edge_Trig, OUTPUT);
+     
   pinMode(R_Echo, INPUT);    
   pinMode(L_Echo, INPUT);
   pinMode(Edge_Echo, INPUT);
@@ -41,22 +38,22 @@ void setup() {
 }
 
 void loop() {
-  // Serial.print("Right");    
-  //Serial.println(SonarSensor(L_Trig, L_Echo));    
-  // Serial.print("Left");     
-  // Serial.println(SonarSensor(R_Trig, R_Echo));
-  // Serial.print("EDGE WARNING");     
-  // Serial.println(SonarSensor(Edge_Trig, Edge_Echo));
+   Serial.print("Right");    
+   Serial.println(SonarSensor(L_Trig, L_Echo));    
+   Serial.print("Left");     
+   Serial.println(SonarSensor(R_Trig, R_Echo));
+   Serial.print("EDGE");     
+   Serial.println(SonarSensor(Edge_Trig, Edge_Echo));
 
 if (SonarSensor(Edge_Trig, Edge_Echo) > 10 && SonarSensor(Edge_Trig, Edge_Echo) > 10){   
   Serial.println("EDGE DETECTED");  
   digitalWrite(stoptritrack, HIGH);
-}
-
-if (SonarSensor(L_Trig, L_Echo) > 15 && SonarSensor(R_Trig, R_Echo) > 15){   
+    }
+  else if (SonarSensor(L_Trig, L_Echo) > 15 && SonarSensor(R_Trig, R_Echo) > 15){   
   Serial.println("SAFE");
+  digitalWrite(forward, HIGH); 
 
-} else if (SonarSensor(L_Trig, L_Echo) <= 11 or SonarSensor(R_Trig, R_Echo) <= 11){
+  } else if (SonarSensor(L_Trig, L_Echo) <= 11 or SonarSensor(R_Trig, R_Echo) <= 11){
   if (SonarSensor(L_Trig, L_Echo) <= 7 && SonarSensor(R_Trig, R_Echo) <= 7){      
     Serial.println("REVERSE");       
     digitalWrite(reverse, HIGH); 
@@ -81,4 +78,4 @@ long SonarSensor(int trigPin,int echoPin){
   digitalWrite(trigPin, LOW); //End Scan  
   duration = pulseIn(echoPin, HIGH); 
   distance = (duration/2) / 29.1;  return distance;  
-}  
+} 
